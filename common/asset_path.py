b91e02bc2a6b462725b06cf7ab7914602d428b98
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-04-07 22:08:15 +0100

    Restore image

--- common/asset_path.py
+++ common/asset_path.py
@@ -10,7 +10,7 @@ def asset_path(app_name):
         apps = []
     ass_path = ""
 
-    if f"github_user_tildagon_{app_name}" in apps:
+    if f"pikesley_tildagon_{app_name}" in apps:
         ass_path = f"/apps/pikesley_tildagon_{app_name}/"
 
     if app_name in apps:
